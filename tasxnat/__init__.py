36e4a53f90320ed20ea3c7f16c4b8390047f7f2c
WilkinsonK <keenanwilkinson@outlook.com>
2023-04-16 22:10:35 -0500

    update structure

--- tasxnat/__init__.py
+++ tasxnat/__init__.py
@@ -1,3 +1,3 @@
-__version__ = (0, 0, 4)
+__version__ = (0, 0, 5)
 
-from tasxnat.tasks import TaskBroker, Taskable, SimpleTaskBroker, SimpleTask
+from tasxnat.objects import TaskBroker, Taskable, SimpleTaskBroker, SimpleTask
