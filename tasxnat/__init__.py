4af53c549425a7310477d0211d399a0c8be5a092
WilkinsonK <keenanwilkinson@outlook.com>
2023-04-18 09:51:10 -0500

    add doctring

--- tasxnat/__init__.py
+++ tasxnat/__init__.py
@@ -1,8 +1,7 @@
-__version__ = (0, 0, 5)
-
-from tasxnat.protocols import Taskable, TaskBroker
-from tasxnat.objects import SimpleTaskable, SimpleTaskBroker
-
+"""
+Simple tasking interface. Objects in this package
+are used to broker calls to registered tasks.
+"""
 
 __all__ = (
     (
@@ -11,4 +10,7 @@ __all__ = (
         "SimpleTaskable",
         "SimpleTaskBroker"
     ))
+__version__ = (0, 0, 5)
 
+from tasxnat.protocols import Taskable, TaskBroker
+from tasxnat.objects import SimpleTaskable, SimpleTaskBroker
