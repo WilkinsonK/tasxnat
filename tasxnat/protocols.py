8261b1be50186d92ce36af2116c0b62fcd76430e
WilkinsonK <keenanwilkinson@outlook.com>
2023-05-29 15:20:02 -0500

    fix improper syntax

--- tasxnat/protocols.py
+++ tasxnat/protocols.py
@@ -12,12 +12,12 @@ _Rt_co = typing.TypeVar("_Rt_co", covariant=True)
 
 
 class TaskI(typing.Protocol[_Ps, _Rt_co]):
-    ...
+    pass
 
 
 class TaskBrokerI(typing.Protocol):
-    ...
+    pass
 
 
 class TaskResultI(typing.Protocol[_Rt_co]):
-    ...
+    pass
